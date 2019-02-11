8dd90539cf36ec2153774a25ac616643beb3070c
smchirila <aluy2893781g@ieselcaminas.org>
2019-02-11 12:25:03 +0100

    commit 3

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const Settings = require('./settings');
 const server = new Hapi.Server({ port: Settings.port });
 const Routes = require('./lib/route');
 const Models = require('./lib/models/');
-server.route(Routes);
+const Path = require('path');
+const Pug = require('pug');
+const Vision = require('vision');
+const Inert = require('inert');
+
+//server.route(Routes);
 
 /*server.route({
     method: 'GET',
@@ -12,10 +17,22 @@ server.route(Routes);
     handler: (request, h) => 'Hello, World!'
 });
 */
+
 const init = async () => {
+    await server.register([Vision, Inert]);
+    server.views({
+        engines: { pug: Pug },
+        path: Path.join(__dirname, 'lib/views'),
+        compileOptions: {
+            pretty: false,
+        },
+        isCached: Settings.env === 'production'
+    });
+    server.route(Routes);
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
+
 process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
